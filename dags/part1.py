93f60afb79ea499827e9f0e4cc1fff7c2989f8c3
Austin Hacker <aehacker@us.ibm.com>
2018-02-10 08:58:15 -0500

    add notes on how to run to first dag

--- dags/part1.py
+++ dags/part1.py
@@ -20,6 +20,13 @@ substitutions = {
 def get_input_file():
     return "/usr/local/luigi/datafiles/example1.txt"
 
+# write your task classees here
+# when you are done, you can run them using docker exec pytn2018luigi_scheduler_1 python3 dags/part1.py <TaskClassName>
+# the visualizer is located at localhost:8082 where you can check on your dag's progress
+#
+# tip: the output file you write to should be located in /usr/local/luigi
+# you can read it using docker exec pytn2018luigi_scheduler_1 cat <your_output_file>
+
 
 if __name__ == "__main__":
     luigi.run()
